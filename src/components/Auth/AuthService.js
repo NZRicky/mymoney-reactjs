9dde9c3a1bcc7dad8f1c85ef9c313059884b6869
Ricky Li <nzrickyli@gmail.com>
2020-02-22 19:45:01 +1300

    add functions to validate if logged in

---
+++ src/components/Auth/AuthService.js
@@ -0,0 +1,46 @@
+import decode from 'jwt-decode';
+
+export default class AuthService {
+    constructor(baseUrl) {
+        this.baseUrl = baseUrl ||  'http://localhost:8080';
+    }
+
+    getToken() {
+        return localStorage.getItem('token');
+    }
+
+    setToken(token) {
+        localStorage.setItem('token', token);
+    }
+
+    isTokenExpired(token) {
+        try {
+            const decoded = decode(token);
+            if (decoded.exp < Date.now() / 1000) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (err) {
+            return false;
+        }
+    }
+    isLoggedIn() {
+        const token = this.getToken();
+        return token && !this.isTokenExpired(token);
+    }
+    fetch(url, options) {
+        const headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        headers.append('Accept', 'applicatioin/json');
+
+        return fetch(url, {
+            headers,
+            ...options
+        });
+    }
+
+    login(username, password) {
+        
+    }
+}
