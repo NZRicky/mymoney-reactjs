12b5bed6d4eb9bcf8a859ebc53ba8ea3c98a6363
Ricky Li <nzrickyli@gmail.com>
2020-02-23 17:42:48 +1300

    login form

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import AuthService from '../Auth/AuthService';
 import { withRouter } from "react-router-dom";
 
+
 class Login extends Component {
     constructor(props) {
         super(props);
